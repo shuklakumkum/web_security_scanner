ffca56ba5441a2513ce3562a4e95f8e106a1c1b2
shukalkumkum1710@gmail.com <shuklakumkum1710@gmail.com>
2026-02-19 12:11:14 +0530

    Initial empty branch

--- src/services/advanced_detection.py
+++
@@ -1,9 +0,0 @@
-def advanced_scan(url: str) -> dict:
-    return {
-        "advanced_checks": {
-            "ip_in_url": False,
-            "phishing_patterns": False,
-        },
-        "risk_score": 0,
-        "warnings": [],
-    }
--- src/services/pattern_checker.py
+++
@@ -1,6 +0,0 @@
-def check_suspicious_patterns(url: str):
-    return {
-        "has_suspicious_patterns": False,
-        "details": []
-    }
-
